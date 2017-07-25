f25218141a535a99dd688a50da64843f0f716f9f
JounQin <admin@1stg.me>
2017-07-25 10:33:23 +0800

    add banner

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,17 @@ import buble from 'rollup-plugin-buble'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
+const pkg = require('./package.json')
+
 export default {
+  banner: `/*!
+ * ${pkg.name} ${pkg.description}
+ * Version ${pkg.version}
+ * Copyright (C) 2017 JounQin <admin@1stg.me>
+ * Released under the MIT license
+ *
+ * Github: https://github.com/JounQin/tpls2js
+ */`,
   entry: 'src/index.js',
   dest: 'dist/tpls2js.js',
   format: 'cjs',
